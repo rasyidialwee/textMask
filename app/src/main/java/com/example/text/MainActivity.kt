af1b67865816734110e19f67ab7834a17824b5b6
Rasyidi Mac <rasyidialwee@gmail.com>
2019-12-10 15:30:14 +0800

    updated to change the flag format

--- app/src/main/java/com/example/text/MainActivity.kt
+++ app/src/main/java/com/example/text/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        decodeToString("Q1RGSUNPTntOZXRCeXRlU2VjfQ==")
+        decodeToString("Q1RGSUNPTntSM3ZlciQzTkJTfQ==")
     }
 
     fun decodeToString(encodedString : String){
